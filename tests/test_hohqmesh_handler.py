b3859565370869eedac206c95fcf361402a3b5d3
Zac Pyle <zpyle8687@sdsu.edu>
2026-01-27 12:27:38 -0800

    Topic: Adding HOHQMesh Reader
    Description:
       * Added src/dingus/mesh/hohwmesh_handler.py. This script contains
         functions for reading in a HOHQMesh mesh file and outputting a
         dictionary containing the mesh information.
    
       * HOHQMesh is a Fortran-based mesher created by David Kopriva
         (See 'Implementing Spectral Methods for Partial Differential
         Equations') specifically for convenient use in spectral solvers.
         Because of this, HOHQMesh files will be the primary mesh type used
         in the DINGUS solver. Support for GMSH will be continued later;
         ideally hoqmesh_handler.py and gmsh_handler.py will be used to
         fill out Element and Mortar classes.
    
       * There are several mesh formats that HOHQMesh can create; one should
         always use the ISM-v2 format (see
         https://github.com/trixi-framework/HOHQMesh). Support for the:
         - ISM
         - ISM-MM
         - ABAQUS
         formats will be added in the future.
    
       * As of this commit, the ISM-v2 HOHQMesh mesh files are successfully
         read in by hohqmesh_handler.py and tested by
         test_hohqmesh_handler.py. The output of hohqmesh_handler.py is a
         dictionary containing the node, element, connectivity, and boundary
         condition data of the mesh.

---
+++ tests/test_hohqmesh_handler.py
@@ -0,0 +1,69 @@
+import dingus.mesh.hohqmesh_handler as hoq
+from pathlib import Path
+import numpy as np
+
+TESTS_INPUT  = Path(__file__).resolve().parent / "testInputs" 
+TESTS_OUTPUT = Path(__file__).resolve().parent / "testOutputs"
+
+def test_read_hohqmesh_ism():
+    """
+    Test the reading of a HOHQMesh mesh file in ISM format.
+    """
+    try:
+        hoq.read_hohqmesh(TESTS_INPUT / "2D" / "Square_ISM.mesh")
+    except Exception as e:
+        errorType = type(e)
+        errorMsg  = str(e)
+        assert errorType == NotImplementedError
+        assert errorMsg == "ISM and ISM-MM format not implemented for HOHQMesh mesh files!"
+
+def test_read_hohqmesh_ism_mm():
+    """
+    Test the reading of a HOHQMesh mesh file in ISM-MM format.
+    """
+    try:
+        hoq.read_hohqmesh(TESTS_INPUT / "2D" / "Square_ISMMM.mesh")
+    except Exception as e:
+        errorType = type(e)
+        errorMsg  = str(e)
+        assert errorType == NotImplementedError
+        assert errorMsg == "ISM and ISM-MM format not implemented for HOHQMesh mesh files!"
+
+def test_read_hohqmesh_abaqus():
+    """
+    Test the reading of a HOHQMesh mesh file in ABAQUS format.
+    """
+    try:
+        hoq.read_hohqmesh(TESTS_INPUT / "2D" / "Square_ABAQUS.mesh")
+    except Exception as e:
+        errorType = type(e)
+        errorMsg  = str(e)
+        assert errorType == NotImplementedError
+        assert errorMsg == "ABAQUS format not implemented for HOHQMesh mesh files!"
+
+def test_read_hohqmesh_ism_v2():
+    """
+    Test the reading of a HOHQMesh mesh file in ISM-V2 format.
+    """
+
+    # Call the reader; output is a dictionary
+    meshData = hoq.read_hohqmesh(TESTS_INPUT / "2D" / "Square_ISMV2.mesh")
+    
+    # Check that the header was read in correctly
+    assert meshData["format"]         == "ISM-V2"
+    assert meshData["num_nodes"]      == 36
+    assert meshData["num_mortars"] == 60
+    assert meshData["num_elements"]   == 25
+    assert meshData["bc_poly_order"]  == 8
+
+    # Check that the nodes were read in correctly
+    assert meshData["nodes"].shape   == (meshData["num_nodes"], 3)
+    
+
+    # Check that all mortars were read in
+    assert meshData["mortars"].shape == (meshData["num_mortars"], 6)
+
+    # Check that the element data (node IDs, mortar curves, BCs) were read in correctly
+    assert meshData["elementNodeIDs"].shape      == (meshData["num_elements"], 4)
+    assert meshData["mortarCurvatureType"].shape == (meshData["num_elements"], 4)
+    assert meshData["elementBCNames"].shape      == (meshData["num_elements"], 4)
